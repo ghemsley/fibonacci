c7196b8cca6b867679d9b8e4e02495698244e243
ghemsley <ghemsley@protonmail.ch>
2017-09-04 13:24:44 -0500

    Added fib.py
    fib.py: defaults to running an interactive fibonacci sequence calculator
    - accepts optional "--index" command line argument for non-interactive use

---
+++ fib.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+def getInput():
+
+    __fibNum = None
+
+    print("Please enter the Fibonacci sequence index to find the value of: ")
+    try:
+        __fibNum = abs(int(input()))
+    except Exception:
+        print("\nThat doesn't seem like a valid entry. Please enter a positive integer.\n")
+        getInput()
+    return __fibNum
+
+def fib(n):
+
+    from math import sqrt
+
+    __n = n
+
+    return int(((1+sqrt(5))**__n-(1-sqrt(5))**__n)/(2**__n*sqrt(5)))
+
+def main():
+
+    import argparse
+    from sys import exit
+
+    __parser = argparse.ArgumentParser()
+    __parser.add_argument("-i", "--index", help="Fibonacci sequence index to find value of", type=int)
+    __args = __parser.parse_args()
+
+    if __args.index or __args.index==0:
+        print(str(fib(abs(int(__args.index)))))
+    else:
+        print("Fibonacci sequence result: " + str(fib(getInput())))
+    exit()
+
+main()
